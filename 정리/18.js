1dcc38c12a974cf95239a3ed9da7501379de52ac
HanHyunJin-mis <forinpea121@gmail.com>
2019-10-21 17:56:42 +0900

    Feat : modified prototype

--- 정리/18.js
+++ 정리/18.js
@@ -1,17 +1,22 @@
+// function Person(name) {
+//   this.name = name;
+//   this.getName = function () {
+//     return this.name;
+//   }
+// }
+
 function Person(name) {
   this.name = name;
-  this.getName = function () {
-    return this.name;
-  }
 }
-
-
-function Person(name) {
-  this.name = name;
-}
-Person.prototype.getName = function () {
+Person.prototype.getName = function() {
   return this.name;
 };
 
-const me = new Person('Lee');
-const you = new Person('Kim');
+const me = new Person("Lee");
+me.__proto__.setName = function() {
+  return "set " + this.name;
+};
+const you = new Person("Kim");
+
+console.log(me.getName());
+console.log(you.__proto__.setName());
